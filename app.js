4f2e0e591578691ff8bb212874d580a92be0a40c
Andressa <andreessafariass@gmail.com>
2025-01-13 15:33:05 -0300

    vercel.json

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const port = 5000;
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
     res.render('index');
